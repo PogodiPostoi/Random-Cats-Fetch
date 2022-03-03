08e03fa754b7098fe892a6ab3acdad631f9c1f42
Artyom Pogodin <boombox577@gmail.com>
2022-03-03 23:50:07 +0300

    adding header and trying to add burger menu

--- js/script.js
+++ js/script.js
@@ -15,4 +15,19 @@ const fetchHandler = async(url) => {
 
 button.addEventListener('click', () => fetchHandler(url))
 
+/* burger menu
+const burgerCheckbox = document.getElementById('burger-checkbox')
+const body = document.body
 
+const burgerCheckboxChecked = (checkbox, element) => {
+    if (checkbox.checked = true) {
+        checkbox.checked = false
+        element.style.overflow = "hidden"
+    } else if (checkbox.checked = false) {
+        checkbox.checked = true
+        element.style.overflow = "auto"
+    } 
+}
+
+burgerCheckbox.addEventListener('click', () => burgerCheckboxChecked(burgerCheckbox, body))
+*/

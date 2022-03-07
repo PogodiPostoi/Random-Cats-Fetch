d095d3320d21d94078746cbaf40b148f37a16db0
Artyom Pogodin <boombox577@gmail.com>
2022-03-07 20:49:39 +0300

    Попытка добавления прелоадера для картинок

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const menu = document.getElementById('menu__toggle')
 
 // Функция по получению картинок из url
 const fetchHandler = async(url) => {
+    console.log(image.src)
     try {
         const response = await fetch(url)
         const json = await response.json()
@@ -14,7 +15,15 @@ const fetchHandler = async(url) => {
         alert('К сожалению, котиков не будет :(')
     }
 }
-button.addEventListener('click', () => fetchHandler(url))
+
+button.addEventListener('click', () => {
+    image.src = 'img/preloadCat.gif'
+    let isLoaded = image.complete
+    if (isLoaded) {
+        fetchHandler(url)
+    }
+})
+
 
 // Бургер-меню. Запрет на скролл при активном меню
-menu.addEventListener('click', () =>  document.body.classList.toggle('noOverflow'))
+menu.addEventListener('click', () =>  document.body.classList.toggle('noScroll'))

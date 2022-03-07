6c5902be31a22860c8ea684a89529a29615c3044
Artyom Pogodin <boombox577@gmail.com>
2022-03-07 14:21:27 +0300

    Отключил скролл при открытии меню и убрал SVG

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const button = document.getElementById('button')
 const image = document.getElementById('img')
 const url = 'https://aws.random.cat/meow'
+const menu = document.getElementById('menu__toggle')
 
+// Функция по получению картинок из url
 const fetchHandler = async(url) => {
     try {
         const response = await fetch(url)
@@ -12,22 +14,7 @@ const fetchHandler = async(url) => {
         alert('К сожалению, котиков не будет :(')
     }
 }
-
 button.addEventListener('click', () => fetchHandler(url))
 
-/* burger menu
-const burgerCheckbox = document.getElementById('burger-checkbox')
-const body = document.body
-
-const burgerCheckboxChecked = (checkbox, element) => {
-    if (checkbox.checked = true) {
-        checkbox.checked = false
-        element.style.overflow = "hidden"
-    } else if (checkbox.checked = false) {
-        checkbox.checked = true
-        element.style.overflow = "auto"
-    } 
-}
-
-burgerCheckbox.addEventListener('click', () => burgerCheckboxChecked(burgerCheckbox, body))
-*/
+// Бургер-меню. Запрет на скролл при активном меню
+menu.addEventListener('click', () =>  document.body.classList.toggle('noOverflow'))
